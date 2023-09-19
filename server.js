bf18abe3e52d3b87d629ee24f4502b969420bd89
Harisjutt15 <mharis1027@gmail.com>
2023-09-19 08:11:15 -0700

    landingpage updated

--- server.js
+++ server.js
@@ -23,193 +23,193 @@ app.use(bodyParser.json());
 var router=require('./Controllers/contact_controller.js')
 
 /////////roles/////////////
-// app.get('/', function (req, res) {
-//     var skills = [
-//         {
-//             btnname: 'ASP.NET',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/net.svg",
-//         },
-//         {
-//             btnname: 'Node / React',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/react-js.svg",
-//         },
-//         {
-//             btnname: 'Laravel',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/laravel.svg",
-//         },
-//         {
-//             btnname: 'MY SQL',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/sql.svg",
-//         },
-//         {
-//             btnname: 'Java',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/java.svg",
-//         },
-//         {
-//             btnname: 'iOS',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/ios.svg",
-//         },
-//         {
-//             btnname: 'React Native',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/react-native.svg",
-//         },
-//         {
-//             btnname: 'React.js',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/react-native.svg",
-//         },
-//         {
-//             btnname: 'Node.js',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/react-js.svg",
-//         },
-//         {
-//             btnname: 'Angular / Node',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/angular.svg",
-//         },
-//         {
-//             btnname: 'PHP',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/php.svg",
-//         }, {
-//             btnname: 'Python / React',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/react-native.svg",
-//         },
-//         {
-//             btnname: 'Ruby on Rails',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/ruby.svg",
-//         },
-//         {
-//             btnname: 'AI / ML',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/ai-ml.svg",
-//         },
-//         {
-//             btnname: 'Android',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/android.svg",
-//         },
-//         {
-//             btnname: 'AWS',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/aws.svg",
-//         },
+app.get('/', function (req, res) {
+    var skills = [
+        {
+            btnname: 'ASP.NET',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/net.svg",
+        },
+        {
+            btnname: 'Node / React',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/react-js.svg",
+        },
+        {
+            btnname: 'Laravel',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/laravel.svg",
+        },
+        {
+            btnname: 'MY SQL',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/sql.svg",
+        },
+        {
+            btnname: 'Java',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/java.svg",
+        },
+        {
+            btnname: 'iOS',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/ios.svg",
+        },
+        {
+            btnname: 'React Native',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/react-native.svg",
+        },
+        {
+            btnname: 'React.js',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/react-native.svg",
+        },
+        {
+            btnname: 'Node.js',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/react-js.svg",
+        },
+        {
+            btnname: 'Angular / Node',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/angular.svg",
+        },
+        {
+            btnname: 'PHP',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/php.svg",
+        }, {
+            btnname: 'Python / React',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/react-native.svg",
+        },
+        {
+            btnname: 'Ruby on Rails',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/ruby.svg",
+        },
+        {
+            btnname: 'AI / ML',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/ai-ml.svg",
+        },
+        {
+            btnname: 'Android',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/android.svg",
+        },
+        {
+            btnname: 'AWS',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/aws.svg",
+        },
 
-//         {
-//             btnname: 'JavaScript',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/javascriptt.svg",
-//         },
-//         {
-//             btnname: 'TypeScript',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/typescript.svg",
-//         },
-//         {
-//             btnname: 'Postgre SQL',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/postgre.svg",
-//         },
-//         {
-//             btnname: 'Python',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/python.svg",
-//         },
-//     ];
-//     var technologies = [
-//         {
-//             btnname: 'Full-Stack',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/full-stack.svg",
-//         },
-//         {
-//             btnname: 'Data Scientist',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/data.svg",
-//         },
-//         {
-//             btnname: 'Mobile',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/react.svg",
-//         },
-//         {
-//             btnname: 'ML Scientist',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/ml-cv.svg",
-//         },
-//         {
-//             btnname: 'Golang Developer',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/golang.svg",
-//         },
-//         {
-//             btnname: 'NLP',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/nlp.svg",
-//         },
-//         {
-//             btnname: 'HTML',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/html.svg",
-//         },
-//         {
-//             btnname: 'Backend',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/backend.svg",
-//         },
-//         {
-//             btnname: 'Data Engineer',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/hadoop.svg",
-//         },
-//         {
-//             btnname: 'Business Analyst',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//             width:28,
+        {
+            btnname: 'JavaScript',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/javascriptt.svg",
+        },
+        {
+            btnname: 'TypeScript',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/typescript.svg",
+        },
+        {
+            btnname: 'Postgre SQL',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/postgre.svg",
+        },
+        {
+            btnname: 'Python',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/python.svg",
+        },
+    ];
+    var technologies = [
+        {
+            btnname: 'Full-Stack',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/full-stack.svg",
+        },
+        {
+            btnname: 'Data Scientist',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/data.svg",
+        },
+        {
+            btnname: 'Mobile',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/react.svg",
+        },
+        {
+            btnname: 'ML Scientist',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/ml-cv.svg",
+        },
+        {
+            btnname: 'Golang Developer',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/golang.svg",
+        },
+        {
+            btnname: 'NLP',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/nlp.svg",
+        },
+        {
+            btnname: 'HTML',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/html.svg",
+        },
+        {
+            btnname: 'Backend',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/backend.svg",
+        },
+        {
+            btnname: 'Data Engineer',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/hadoop.svg",
+        },
+        {
+            btnname: 'Business Analyst',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+            width:28,
            
            
-//         },
-//         {
-//             btnname: 'Frontend',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/front-end.svg",
-//         }, {
-//             btnname: 'DevOps',
-//             img: "https://letsremotify.com/wp-content/uploads/2023/05/devops.svg",
-//         },
-//     ];
-//   var trajectory=[
-//     {
-//         btnname: 'Software Engineer',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28,
-//     },
-//     {
-//         btnname: 'Senior Architect',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28,
-//     },
-//     {
-//         btnname: 'Senior Engineer',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28
-//     },
-//     {
-//         btnname: 'Marketer',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28
-//     },
-//     {
-//         btnname: 'Software Developer',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28
-//     },
-//     {
-//         btnname: 'Project Manager ',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28
-//     },
-//     {
-//         btnname: 'Software Architect',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28,
+        },
+        {
+            btnname: 'Frontend',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/front-end.svg",
+        }, {
+            btnname: 'DevOps',
+            img: "https://letsremotify.com/wp-content/uploads/2023/05/devops.svg",
+        },
+    ];
+  var trajectory=[
+    {
+        btnname: 'Software Engineer',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28,
+    },
+    {
+        btnname: 'Senior Architect',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28,
+    },
+    {
+        btnname: 'Senior Engineer',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28
+    },
+    {
+        btnname: 'Marketer',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28
+    },
+    {
+        btnname: 'Software Developer',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28
+    },
+    {
+        btnname: 'Project Manager ',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28
+    },
+    {
+        btnname: 'Software Architect',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28,
       
-//     },
-//     {
-//         btnname: 'Tech Lead Manager',
-//         img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
-//         width:28,
+    },
+    {
+        btnname: 'Tech Lead Manager',
+        img: "https://letsremotify.com/wp-content/uploads/2023/05/js.svg",
+        width:28,
        
-//     }
+    }
 
-//   ]
+  ]
 
-//     res.render('index.ejs', {
-//         technologies: technologies,
-//         skills:skills,
-//         trajectory:trajectory
-//     });
-// });
+    res.render('index.ejs', {
+        technologies: technologies,
+        skills:skills,
+        trajectory:trajectory
+    });
+});
 ////////skills
 
 app.use('/', router);
